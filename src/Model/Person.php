5acffb1e22ec270ea9d254a8570f47f07d1f31df
Jesse Rushlow <jr@geeshoe.com>
2020-04-27 00:47:36 -0400

    added getters and types to person

--- src/Model/Person.php
+++ src/Model/Person.php
@@ -4,16 +4,36 @@ namespace Rushlow\Bundle\Atom\Model;
 
 class Person
 {
-    private $id;
-    private $name;
-    private $uri;
-    private $email;
+    private string $id;
+    private string $name;
+    private ?string $uri;
+    private ?string $email;
 
     public function __construct(string $id, string $name, string $uri = null, string $email = null)
     {
         $this->id = $id;
         $this->name = $name;
-        $this->uri = $uri;
         $this->email = $email;
+        $this->uri = $uri;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function getUri(): ?string
+    {
+        return $this->uri;
     }
 }
